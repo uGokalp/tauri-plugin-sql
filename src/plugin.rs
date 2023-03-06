c995f95d6b7df9c69c77d7af04e1a3e54cbca625
Umur Gokalp <15ug1@queensu.ca>
2023-03-06 23:48:44 +0300

    Update plugin.rs
    use resource_dir instead of app_dir

--- src/plugin.rs
+++ src/plugin.rs
@@ -65,7 +65,7 @@ type Result<T> = std::result::Result<T, Error>;
 fn app_path<R: Runtime>(app: &AppHandle<R>) -> PathBuf {
     #[allow(deprecated)] // FIXME: Change to non-deprecated function in Tauri v2
     app.path_resolver()
-        .app_dir()
+        .resource_dir()
         .expect("No App path was found!")
 }
 
